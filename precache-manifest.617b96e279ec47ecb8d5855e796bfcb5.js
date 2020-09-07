cd3be368f71f0e91b28a45b9aaf68b514c375dc4
Littletittlecatcat <yw.chen.michael@gmail.com>
2020-09-07 17:16:10 +0800

    Updates

---
+++ precache-manifest.617b96e279ec47ecb8d5855e796bfcb5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4285907cb94f79dd92735fdf7940a9b",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "621635d2a5e232e70366",
+    "url": "/resume/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "b6828f87edf2a1c0a5ec",
+    "url": "/resume/static/js/2.f21f2039.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/resume/static/js/2.f21f2039.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "621635d2a5e232e70366",
+    "url": "/resume/static/js/main.b146b5cd.chunk.js"
+  },
+  {
+    "revision": "bcaa68c0b5f7adc089f7",
+    "url": "/resume/static/js/runtime-main.05fbbc33.js"
+  },
+  {
+    "revision": "9c3037cc702a7f8681db0e4e13488961",
+    "url": "/resume/static/media/artemis.9c3037cc.jpg"
+  },
+  {
+    "revision": "cfc5737c9eaea3a37bfa6a2def122dfb",
+    "url": "/resume/static/media/book thief.cfc5737c.jpg"
+  },
+  {
+    "revision": "80607fd2b90e3ecbba6f6c8fbbb98091",
+    "url": "/resume/static/media/flipped.80607fd2.jpg"
+  },
+  {
+    "revision": "116b6857532e00e6b058670cf84f4861",
+    "url": "/resume/static/media/oneday.116b6857.jpg"
+  },
+  {
+    "revision": "5befb086cb19b395e473b1ee84c22614",
+    "url": "/resume/static/media/the time machine.5befb086.jpg"
+  }
+]);
