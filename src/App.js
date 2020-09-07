1ba3ae1821904e4812b4a98e55294786f8ac185b
Littletittlecatcat <yw.chen.michael@gmail.com>
2020-09-07 20:00:34 +0800

    make changes 1

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
 
     getResumeData() {
         $.ajax({
-            url: '/resumeData.json',
+            url: './resumeData.json',
             dataType: 'json',
             cache: false,
             success: function (data) {
